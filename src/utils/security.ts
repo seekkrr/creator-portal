ad61ff901583198a5bde95b7a11e1d19315dd78f
Rishabh Maheshwari <rishphalod7@gmail.com>
2026-02-09 00:03:31 +0530

    Fixed changes

---
+++ src/utils/security.ts
@@ -0,0 +1,11 @@
+export function escapeHtml(unsafe: string | undefined | null): string {
+    if (unsafe === undefined || unsafe === null) {
+        return '';
+    }
+    return unsafe
+        .replace(/&/g, '&amp;')
+        .replace(/</g, '&lt;')
+        .replace(/>/g, '&gt;')
+        .replace(/"/g, '&quot;')
+        .replace(/'/g, '&#039;');
+}
